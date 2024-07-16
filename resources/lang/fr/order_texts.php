342f0d5a70c6af4be292246c8cd4c176d7f87df5
Eloge <info@noussape.com>
2024-07-16 15:27:20 +0000

    admin noussape28 commit

---
+++ resources/lang/fr/order_texts.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+       'searching_for_delivery_man'=>'Recherche d\'un livreur',
+    'accepted_by_delivery_man'=>'Accepté par le livreur',
+    'preparing_in_restaurants'=>'En préparation',
+    'picked_up'=>'Ramassé',
+    'dashboard_order_statistics'=>'Statistiques de commande sur le tableau de bord',
+];
